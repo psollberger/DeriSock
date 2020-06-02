153011957072a8af0aec82b2e116a73df47136c9
Patrick Sollberger <psollberger83@gmail.com>
2020-06-02 19:36:34 +0200

    Made GrantType strings constant

--- DeriSock/Constants/GrantType.cs
+++ DeriSock/Constants/GrantType.cs
@@ -2,8 +2,8 @@
 {
   public static class GrantType
   {
-    public static string Credentials = "client_credentials";
-    public static string Signature = "client_signature";
-    public static string RefreshToken = "refresh_token";
+    public const string Credentials = "client_credentials";
+    public const string Signature = "client_signature";
+    public const string RefreshToken = "refresh_token";
   }
 }
