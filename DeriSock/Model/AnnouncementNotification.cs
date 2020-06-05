bd364f74b937ccd5527d777e677c73a1f108df27
Patrick Sollberger <psollberger83@gmail.com>
2020-06-05 20:40:24 +0200

    Added first two Subscriptions

--- DeriSock/Model/AnnouncementNotification.cs
+++ DeriSock/Model/AnnouncementNotification.cs
@@ -3,46 +3,58 @@
   using System;
   using Newtonsoft.Json;
 
-  public class Announcement
+  public class AnnouncementNotification
   {
     /// <summary>
-    ///   The HTML body of the announcement
+    /// Action taken by the platform administrators. Published a new announcement, or deleted the old one
+    /// </summary>
+    [JsonProperty("action")]
+    public string Action { get; set; }
+
+    /// <summary>
+    /// HTML-formatted announcement body
     /// </summary>
     [JsonProperty("body")]
     public string Body { get; set; }
 
     /// <summary>
-    ///   Whether the user confirmation is required for this announcement
+    /// Whether the user confirmation is required for this announcement
     /// </summary>
     [JsonProperty("confirmation")]
     public bool Confirmation { get; set; }
 
     /// <summary>
-    ///   A unique identifier for the announcement
+    /// Announcement's identifier
     /// </summary>
     [JsonProperty("id")]
     public long Id { get; set; }
 
     /// <summary>
-    ///   Whether the announcement is marked as important
+    /// Whether the announcement is marked as important
     /// </summary>
     [JsonProperty("important")]
     public bool Important { get; set; }
 
     /// <summary>
-    ///   The timestamp in ms at which the announcement was published
+    /// The timestamp (milliseconds since the Unix epoch) of announcement publication
     /// </summary>
     [JsonProperty("publication_timestamp")]
     public long PublicationTimestamp { get; set; }
 
-    /// <inheritdoc cref="PublicationTimestamp" />
+    /// <inheritdoc cref="PublicationTimestamp"/>
     [JsonIgnore]
     public DateTime PublicationDateTime => PublicationTimestamp.AsDateTimeFromMilliseconds();
 
     /// <summary>
-    ///   The title of the announcement
+    /// Announcement's title
     /// </summary>
     [JsonProperty("title")]
     public string Title { get; set; }
+
+    /// <summary>
+    /// The number of previous unread announcements (optional, only for authorized users).
+    /// </summary>
+    [JsonProperty("unread")]
+    public int Unread { get; set; }
   }
 }
