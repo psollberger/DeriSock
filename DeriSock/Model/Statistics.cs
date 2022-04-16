b5c37daa6f63c99426434030b3c6c08ee507a0b1
Patrick Sollberger <psollberger83@gmail.com>
2022-04-16 08:54:16 +0200

    Ticker stats values can be null - Fixes #25

--- DeriSock/Model/Statistics.cs
+++ DeriSock/Model/Statistics.cs
@@ -8,23 +8,23 @@ public class Statistics
   ///   highest price during 24h
   /// </summary>
   [JsonProperty("high")]
-  public decimal High { get; set; }
+  public decimal? High { get; set; }
 
   /// <summary>
   ///   lowest price during 24h
   /// </summary>
   [JsonProperty("low")]
-  public decimal Low { get; set; }
+  public decimal? Low { get; set; }
 
   /// <summary>
   ///   24-hour price change expressed as a percentage, null if there weren't any trades
   /// </summary>
   [JsonProperty("price_change")]
-  public decimal PriceChange { get; set; }
+  public decimal? PriceChange { get; set; }
 
   /// <summary>
   ///   volume during last 24h in base currency
   /// </summary>
   [JsonProperty("volume")]
-  public decimal Volume { get; set; }
+  public decimal? Volume { get; set; }
 }
